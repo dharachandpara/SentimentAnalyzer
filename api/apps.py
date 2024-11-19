00c31cbfe77e966ca4ce4d6b4e335a1ac8c9455a
farzanaalam22 <farzanaalam11@gmail.com>
2024-11-19 18:33:14 -0500

    convert to full react app

--- api/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from .firebase_service import collection_listener
-from .listener_callback import handle_changes
-
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
